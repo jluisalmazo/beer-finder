a6c8b355f445d9c543a32cb6d6009a7fd51cb4cf
Juan Luis Almazo <jluisalmazo@hotmail.com>
2018-10-05 15:58:13 -0500

    Mocks for categories, venues and venue detail endpoints were added in order to accelerate developement and tests. Some styles were also included.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,18 +15,19 @@ import 'bootstrap/js/dist/modal';
 })
 export class AppComponent implements OnInit {
 
-  title = 'Venues Finder';
+  title = 'Search for Venues';
   venues = [];
   categories = [];
   frmSearchOptions: FormGroup;
   message = '';
   userCoords = '';
+  showCover = true;
 
   constructor(private fb: FormBuilder, public readonly httpRequestsService: HttpRequestsService) {
 
     this.frmSearchOptions = fb.group({
-      'category': [null],
-      'place': [null]
+      'category': [""],
+      'place': [""]
     });
 
   }
@@ -38,21 +39,22 @@ export class AppComponent implements OnInit {
     // let apiCategoriesURL = 'https://api.foursquare.com/v2/venues/categories?' + environment.client_id + '&' + environment.client_secret + '&' + environment.version;
 
     // this.httpRequestsService.sendGetRequest(apiCategoriesURL).subscribe(
-    //   res => {
-    //     console.log(res);
-    //     this.categories = res['response']['categories'];
+    this.httpRequestsService.sendGetRequest("http://localhost:4200/assets/jsons/categories.json").subscribe(
+      res => {
+        console.log(res);
+        this.categories = res['response']['categories'];
 
-    //   },
-    //   (err: HttpErrorResponse) => {
-    //     console.log(err);
-    //     this.message = err['error']['meta']['errorDetail'];
-    //     if (err.status >= 500) {
-    //       this.message = 'Server-side error occured: ' + this.message;
-    //     } else {
-    //       this.message = 'Client-side error occured: ' + this.message;
-    //     }
-    //   }
-    // );
+      },
+      (err: HttpErrorResponse) => {
+        console.log(err);
+        this.message = err['error']['meta']['errorDetail'];
+        if (err.status >= 500) {
+          this.message = 'Server-side error occured: ' + this.message;
+        } else {
+          this.message = 'Client-side error occured: ' + this.message;
+        }
+      }
+    );
 
   }
 
@@ -82,11 +84,13 @@ export class AppComponent implements OnInit {
       apiSearchVenuesURL += '&categoryId=' + category;
     }
 
-    this.httpRequestsService.sendGetRequest(apiSearchVenuesURL + '&limit=50').subscribe(
+    // this.httpRequestsService.sendGetRequest(apiSearchVenuesURL + '&limit=50').subscribe(
+    this.httpRequestsService.sendGetRequest("http://localhost:4200/assets/jsons/venues.json").subscribe(
 
       res => {
         console.log(res);
         this.venues = res['response']['venues'];
+        this.showCover = false;
       },
       (err: HttpErrorResponse) => {
 
@@ -130,41 +134,42 @@ export class AppComponent implements OnInit {
     // let apiGetVenueDetailURL = 'https://api.foursquare.com/v2/venues/' + venueId + '?' + environment.client_id + '&' + environment.client_secret + '&' + environment.version;
 
     // this.httpRequestsService.sendGetRequest(apiGetVenueDetailURL).subscribe(
+    this.httpRequestsService.sendGetRequest("http://localhost:4200/assets/jsons/venueDetail.json").subscribe(
 
-    //   res => {
-    //     console.log(res);
+      res => {
+        console.log(res);
 
-    //     let venueName = res['response']['venue']['name'];
-    //     let venueFormattedAddress = res['response']['venue']['location']['formattedAddress'];
-    //     let venueBestPhoto = res['response']['venue']['bestPhoto'];
+        let venueName = res['response']['venue']['name'];
+        let venueFormattedAddress = res['response']['venue']['location']['formattedAddress'];
+        let venueBestPhoto = res['response']['venue']['bestPhoto'];
 
-    //     let venueBestPhotoURL = venueBestPhoto['prefix'] + 'original' + venueBestPhoto['suffix'];
-
-
-    //     let $venueBestPhotoElement = $("<img>").attr("src", venueBestPhotoURL);
+        let venueBestPhotoURL = venueBestPhoto['prefix'] + 'original' + venueBestPhoto['suffix'];
 
 
-    //     $("#venue-detail .modal-title").html(venueName);
-    //     $("#venue-detail .modal-body").html(venueFormattedAddress).append($venueBestPhotoElement);
-    //     $("#venue-detail").modal('show');
+        let $venueBestPhotoElement = $("<img>").attr("src", venueBestPhotoURL);
 
-    //   },
-    //   (err: HttpErrorResponse) => {
 
-    //     console.log(err);
-    //     let errorMessage = err['error']['meta']['errorDetail'];
-    //     if (err.status >= 500) {
-    //       errorMessage = 'Server-side error occured: ' + errorMessage;
-    //     } else {
-    //       if (err.status == 429) {
-    //         errorMessage = 'Foursquare quota for this endpoint was exceeded. Please, try tomorrow.';
-    //       } else {
-    //         errorMessage = 'Client-side error occured: ' + errorMessage;
-    //       }
-    //     }
-    //     alert(errorMessage);
-    //   }
-    // );
+        $("#venue-detail .modal-title").html(venueName);
+        $("#venue-detail .modal-body").html(venueFormattedAddress).append($venueBestPhotoElement);
+        $("#venue-detail").modal('show');
+
+      },
+      (err: HttpErrorResponse) => {
+
+        console.log(err);
+        let errorMessage = err['error']['meta']['errorDetail'];
+        if (err.status >= 500) {
+          errorMessage = 'Server-side error occured: ' + errorMessage;
+        } else {
+          if (err.status == 429) {
+            errorMessage = 'Foursquare quota for this endpoint was exceeded. Please, try tomorrow.';
+          } else {
+            errorMessage = 'Client-side error occured: ' + errorMessage;
+          }
+        }
+        alert(errorMessage);
+      }
+    );
 
   }
 
